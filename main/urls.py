6de5348f581c9a434bded200f82bff51a5a31718
Abdurrahman YAKAR <ryakar75@gmail.com>
2025-09-22 04:04:52 +0300

    feat: update some files

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "main"  # namespace için (önerilir)
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
