646defeaf3c47bfc019cd270c2aa644240e564d0
Abdurrahman YAKAR <ryakar75@gmail.com>
2026-03-29 16:53:02 +0300

    feat: update some files

--- drnahsap/settings.py
+++ drnahsap/settings.py
@@ -1,18 +1,15 @@
 import os
 from pathlib import Path
-from decouple import config, Csv  # Sadece bunları import etmeniz yeterli
+from decouple import config, Csv
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# --- ENV DOSYASINDAN OKUNAN AYARLAR ---
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -54,64 +51,39 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'drnahsap.wsgi.application'
 
-# Database - docker.env içindeki DATABASE_URL'i kullanır
 DATABASES = {
-    'default': config(
-        'DATABASE_URL',
-        cast=dj_database_url.parse
-    )
+    'default': config('DATABASE_URL', cast=dj_database_url.parse)
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Internationalization
 LANGUAGE_CODE = 'tr'
 TIME_ZONE = 'Europe/Istanbul'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# --- STATIK VE MEDYA AYARLARI ---
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Docker volume ile eşleşen yer
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Session ve Login Ayarları
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-LOGIN_URL = "/yonetici/"
-LOGIN_REDIRECT_URL = "/yonetici/panel/"
-
-# Dosya Yükleme Sınırları
-DATA_UPLOAD_MAX_MEMORY_SIZE = None
-FILE_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 50000
-FILE_UPLOAD_TEMP_DIR = BASE_DIR / "tmp_uploads"
-
-
-import os
-
-# Gmail SMTP Ayarları
-# settings.py içindeki mail kısmını şöyle güncelle:
+# --- MAIL AYARLARI ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
-# os.getenv yerine projenin geri kalanında kullandığın config() fonksiyonunu kullan:
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
 DEFAULT_FROM_EMAIL = f'DRN Ahşap Atölyesi <{EMAIL_HOST_USER}>'
 
-
+# ... Diğer Session ve Limit ayarları aynı kalabilir ...
